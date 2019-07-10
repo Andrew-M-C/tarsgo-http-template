4f53887337a47a23b9624d9d7bba06686352d87f
Andrew-M-C <laplacezhang@126.com>
2019-07-10 14:34:42 +0800

    [*] feat: make the template practical

--- vendor/impl/web/web.go
+++ vendor/impl/web/web.go
@@ -2,6 +2,7 @@ package web
 import(
 	"github.com/TarsCloud/TarsGo/tars"
 	tarsconf "github.com/Andrew-M-C/tarsgo-tools/config"
+	"github.com/Andrew-M-C/tarsgo-tools/sesslog"
 	"net/http"
 	"fmt"
 )
@@ -12,23 +13,32 @@ const (
 )
 
 func AddServant() {
+	l := sesslog.New(OBJ_NAME)
+	defer l.Close()
+
 	cfg := tars.GetServerConfig()
 	servant := cfg.App + "." + cfg.Server + "." + OBJ_NAME
+	l.Debug("servant: %s", servant)
 
 	tarscfg, _ := tarsconf.NewConfig()
 	if tarscfg == nil {
 		return
 	}
 
-	_, exist := tarscfg.GetString(fmt.Sprintf("tars/application/server/%sAdapter", servant), "endpoont", "undefined")
+	endpoint, exist := tarscfg.GetString(fmt.Sprintf("tars/application/server/%sAdapter", servant), "endpoint", "undefined")
+	l.Debug("endpoint: %s", endpoint)
 	if exist {
 		mux := &tars.TarsHttpMux{}
-		mux.HandleFunc("/bin", httpBinHandler)
-		tars.AddHttpServant(mux, cfg.Server + "." + OBJ_NAME)
+		mux.HandleFunc("/", httpBinHandler)
+		tars.AddHttpServant(mux, servant)
+		l.Info("add %s OK", servant)
 	}
 }
 
 func httpBinHandler(w http.ResponseWriter, r *http.Request) {
+	l := sesslog.New(OBJ_NAME + "_bin")
+	defer l.Close()
+	l.Debug("Got request")
 	w.Write([]byte("Hello Tars!"))
 	return
 }
